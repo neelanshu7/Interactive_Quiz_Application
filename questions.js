d2f465064ac4465adf363758fe3d578386e2f85a
Neelanshu Mitra <mitraneelanshu@gmail.com>
2025-07-08 01:45:12 +0530

    Source-code

---
+++ questions.js
@@ -0,0 +1,167 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which property is used to change the background color in CSS?",
+        answer: "background-color",
+        options: [
+            "color",
+            "bgcolor",
+            "background-color",
+            "background"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which CSS property is used to make text bold?",
+        answer: "font-weight",
+        options: [
+            "font-weight",
+            "font-style",
+            "text-decoration",
+            "text-transform"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Which symbol is used for comments in JavaScript?",
+        answer: "//",
+        options: [
+            "//",
+            "/* */",
+            "<!-- -->",
+            "#"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which company developed JavaScript?",
+        answer: "Netscape",
+        options: [
+            "Mozilla",
+            "Microsoft",
+            "Netscape",
+            "Sun Microsystems"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Which of the following is not a programming language?",
+        answer: "HTML",
+        options: [
+            "Python",
+            "Java",
+            "HTML",
+            "C++"
+        ]
+    },
+    {
+        numb: 11,
+        question: "What does API stand for?",
+        answer: "Application Programming Interface",
+        options: [
+            "Application Programming Interface",
+            "Application Protocol Interface",
+            "Applied Program Internet",
+            "Applied Protocol Interface"
+        ]
+    },
+    {
+        numb: 12,
+        question: "Which SQL statement is used to extract data from a database?",
+        answer: "SELECT",
+        options: [
+            "GET",
+            "SELECT",
+            "EXTRACT",
+            "OPEN"
+        ]
+    },
+    {
+        numb: 13,
+        question: "What does DOM stand for?",
+        answer: "Document Object Model",
+        options: [
+            "Document Object Model",
+            "Data Object Model",
+            "Document Oriented Model",
+            "Data Oriented Model"
+        ]
+    },
+    {
+        numb: 14,
+        question: "Which HTML attribute is used to define inline styles?",
+        answer: "style",
+        options: [
+            "style",
+            "class",
+            "font",
+            "styles"
+        ]
+    },
+    {
+        numb: 15,
+        question: "Which CSS property controls the text size?",
+        answer: "font-size",
+        options: [
+            "text-style",
+            "font-style",
+            "font-size",
+            "text-size"
+        ]
+    }
+];
